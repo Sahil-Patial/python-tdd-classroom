8b4a8056b8dc5252a991ec0b4cace2d3b27019a9
Sahil Patial <sahil.patial@datagrokr.com>
2021-07-02 12:19:39 +0530

    functions implemented

--- src/exercises.py
+++ src/exercises.py
@@ -2,10 +2,19 @@ def reverse_list(input_list):
     """
     Reverses order of elements in list input_list.
     """
-    return None
+    input_list.reverse()
+    return input_list
+
+input_list = [5,8,2,1,3,4]
+print(reverse_list(input_list))
+    
 
 def count_digits(number):
     """
     Return count of digits
     """
-    return None
+    number = str(number)
+    return len(number)
+
+number = 1235678
+print(count_digits(number))
--- src/string_exercise.py
+++ src/string_exercise.py
@@ -1,31 +1,51 @@
+import re
 class StringExercise:
 
     def __init__(self):
-        pass   # Do some initial setup in this constructor method, if needed
+           # Do some initial setup in this constructor method, if needed
+           pass
 
     def reverse_string(self, input_str):
         """
         Reverses order of characters in string input_str.
         """
-        return None
+        s = ""
+        for i in input_str:
+            s = i + s
+        return s
 
     def is_english_vowel(self, character):
         """
         Returns True if character is an english vowel
         and False otherwise.
         """
-        return None
+        if (character == 'a' or character == 'e' or character == 'i' or character == 'o' or character == 'u' or character == 'A' or character == 'E' or character == 'I' or character == 'O' or character == 'U'):
+            return True
+        else:
+            return False
 
     def find_longest_word(self, sentence):
         """
         Returns the longest word in string sentence.
         In case there are several, return the first.
         """
-        return None
+        res = re.findall(r"\w+",sentence)
+        n = max(res,key = lambda x : len(x))
+        return n
 
     def get_word_lengths(self, text):
         """
         Returns a list of integers representing
         the word lengths in string text.
         """
-        return None
+        text = text.split(' ')
+        l=[]
+        for words in text:
+            l.append(len(words))
+        return l
+
+a = StringExercise()
+a.reverse_string("sahil")
+a.is_english_vowel("e")
+a.find_longest_word("hello")
+a.get_word_lengths("how are you")
